58ca7f8c9b2ba7781442ee5da4c943c51801006c
Shajee Cuevas <shajeecuevas@gmail.com>
2018-06-02 18:54:26 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav.component';
+import { MainComponent } from './main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/main.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'main-component',
+    template: `
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <p> Hi </p>
+            </div>
+        </div>
+    </div>
+    `,
+    styleUrls: ['./main.component.css']
+})
+export class MainComponent {
+
+}
